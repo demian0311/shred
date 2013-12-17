be44974ba46388b446bf13c6274ee080a19cfd59
Demian Neidetcher <demian0311@gmail.com>
2013-12-17 14:54:57 -0600

    initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,45 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import play.api.libs.json._
+
+
+object Application extends Controller {
+
+  // not production ready
+  var countMap: Map[String, Int] = Map()
+
+  def index = Action {
+    Ok(views.html.index("Hello Play Framework"))
+  }
+
+  def getNode(resource: String) = Action {
+    val count: Int = countMap.getOrElse(resource, 0)
+
+    Ok(Json.obj(
+      "resource" -> resource,
+      "count" -> count
+    ))
+  }
+
+  def postNode(resource: String) = Action {
+    val result: Option[Int] = countMap.get(resource)
+
+    result match {
+      case Some(value: Int) => {
+        val newValue = value + 1
+        countMap += (resource -> newValue)
+        NoContent
+      }
+      case None => {
+        countMap += (resource -> 1)
+        Created(Json.obj( "resource" -> resource ))
+      }
+    }
+  }
+
+  def deleteNode(resource: String) = Action {
+    countMap -= resource
+    NoContent
+  }
+}
---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+RESOURCE=path/to/resource
+URL=http://localhost:9000/$RESOURCE
+
+curl -X POST $URL 
+curl -X POST $URL 
+curl -X POST $URL 
+curl -X POST $URL 
+curl -X POST $URL 
+
+curl $URL; echo "\n"
+
+curl -X DELETE $URL 
+curl -X POST $URL; echo "\n"
+curl $URL; echo "\n"
+
+curl -X DELETE $URL 
+
+
